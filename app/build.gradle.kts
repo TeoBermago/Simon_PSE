8bdc716e858136725ae25574365522a51ca64018
TeoBermago <matteo.bergamo.04.md@gmail.com>
2026-04-17 03:11:38 +0200

    Screen 1 UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.simon_pse"
-        minSdk = 26
+        minSdk = 36
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -48,6 +48,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/simon_pse/MainActivity.kt
+++ app/src/main/java/com/example/simon_pse/MainActivity.kt
@@ -4,44 +4,155 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
+import androidx.compose.ui.graphics.Color
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.height
+import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.simon_pse.ui.theme.Simon_PSETheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
+
+        // Set and display the UI content
         setContent {
             Simon_PSETheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    GameScreen(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+
+}
+
+
+@Composable
+fun GameScreen(modifier: Modifier = Modifier) {
+    Column() {
+        Row(
+            modifier = modifier.fillMaxWidth(). fillMaxHeight(0.5f),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            ColorButtonsGrid()
+        }
+
+        Row(
+            modifier = modifier.fillMaxWidth(). fillMaxHeight(0.5f),
+            //verticalAlignment = Alignment.CenterVertically
+        ) {
+            BottomScreen1()
+        }
+    }
+}
+
+data class ButtonData(
+    val text: String,
+    val backgroundColor: Color,
+    val onClick: () -> Unit
+)
+
+@Composable
+fun StandardButton(text: String, backgroundColor: Color, onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        // Set a standard size \
+        modifier = Modifier
+            .width(150.dp)
+            .height(70.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = backgroundColor,
+            contentColor = Color.Black // Text color
+
+        )
+    ) {
+        Text(text)
+    }
+}
+
+@Composable
+fun ColorButtonsGrid() {
+    val buttons = listOf(
+        ButtonData(stringResource(R.string.red), Color.Red, {}),
+        ButtonData(stringResource(R.string.green), Color.Green, {}),
+        ButtonData(stringResource(R.string.blue), Color.Blue, {}),
+        ButtonData(stringResource(R.string.magenta), Color.Magenta, {}),
+        ButtonData(stringResource(R.string.yellow), Color.Yellow, {}),
+        ButtonData(stringResource(R.string.cyan), Color.Cyan, {})
+    )
+
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(15.dp)
+    ) {
+        // Dividiamo la lista in righe da 2
+        buttons.chunked(2).forEach { riga ->
+            Row(modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(10.dp, Alignment.CenterHorizontally)
+            ) {
+                riga.forEach { (title, color, action) ->
+                    StandardButton(
+                        text = title,
+                        backgroundColor = color,
+                        onClick = action
                     )
                 }
             }
         }
     }
+
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun BottomScreen1() {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        Text(
+
+            text = "banana"
+        )
+        Row(
+
+            horizontalArrangement = Arrangement.spacedBy(15.dp)
+        ) {
+            StandardButton("Cancella", Color.Gray, {})
+            StandardButton("Fine Partita", Color.Gray, {})        }
+    }
+
 }
 
-@Preview(showBackground = true)
+
+
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
+fun DashboardPreview() {
     Simon_PSETheme {
-        Greeting("Android")
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            GameScreen(modifier = Modifier.padding(innerPadding))
+        }
     }
 }
+
